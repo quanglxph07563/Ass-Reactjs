acf582866d74894482b1f64dc521803e3ed8c5b4
unknown <quanglxph07563@fpt.edu.vn>
2020-08-05 23:53:46 +0700

    loc san pham

--- src/pages/views/Admin/Products/ShowProduct.js
+++ src/pages/views/Admin/Products/ShowProduct.js
@@ -1,20 +1,28 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useRef } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
 import swal from "sweetalert";
-
+import http from "../../../../api/api.js";
 const ShowProducts = ({ addFormData }) => {
   const [items, setItems] = useState([]);
   const [itemsForm, setItemsForm] = useState({});
-  const [page, setPage] = useState(1);
+  const [page, setPage] = useState({
+    page_size: 3,
+    page: 1,
+    iddm: null,
+    key: '',
+  });
   const [totalpage, setTotalpage] = useState(1);
   const [menuItemsShow, setmenuItems] = useState([]);
+  const [category, setCategory] = useState([]);
 
-  var stt = 4 * (page - 1) + 1;
-
+  const setTimeSearch = useRef(null)
+  var stt = page.page_size * (page.page - 1) + 1;
   const setList = () => {
-    axios
-      .get(`http://127.0.0.1:8000/api/products/?page=${page}`)
+    http
+      .get(
+        `products?page=${page.page}&page_size=${page.page_size}&iddm=${page.iddm}&key=${page.key}`
+      )
       .then(function (response) {
         setItems(response.data.data);
         setTotalpage(response.data.last_page);
@@ -28,8 +36,8 @@ const ShowProducts = ({ addFormData }) => {
 
   useEffect(setList, [page]);
   const deleteProduct = (id) => {
-    axios
-      .delete("http://127.0.0.1:8000/api/products/" + id)
+    http
+      .delete("products/" + id)
       .then(function (response) {
         setList();
         swal({
@@ -47,12 +55,15 @@ const ShowProducts = ({ addFormData }) => {
   let menuItems = [];
 
   const chuyenPage = (e) => {
-    setPage(Number(e.target.getAttribute("page")));
+    setPage({
+      ...page,
+      page: Number(e.target.getAttribute("page")),
+    });
   };
   const pagination = (totalpage) => {
     // console.log(totalpage);
     for (let index = 1; index < totalpage + 1; index++) {
-      if (page == index) {
+      if (page.page == index) {
         menuItems.push(
           <li className="page-item active">
             <a className="page-link" page={index} onClick={chuyenPage}>
@@ -72,9 +83,15 @@ const ShowProducts = ({ addFormData }) => {
     }
     setmenuItems([menuItems]);
   };
-
+  if (document.getElementById("checkAll") != null) {
+    document.getElementById("checkAll").checked = false;
+    var check = document.getElementsByClassName("checkbox");
+    for (let index = 0; index < check.length; index++) {
+      check[index].checked = false;
+    }
+  }
   const checkAll = () => {
-  let i = 0;
+    let i = 0;
     var check = document.getElementsByClassName("checkbox");
     for (let index = 0; index < check.length; index++) {
       if (check[index].checked) {
@@ -100,21 +117,122 @@ const ShowProducts = ({ addFormData }) => {
       document.getElementById("checkAll").checked = false;
     }
   };
+  const getcategory = () => {
+    http
+      .get("get-all-category")
+      .then(function (response) {
+        setCategory(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  };
+  useEffect(getcategory, []);
+  const searchKey = (e) => {
+    var keySearch = e.target.value
+    if(setTimeSearch.current){
+      clearTimeout(setTimeSearch.current)
+    }
+    setTimeSearch.current= setTimeout(() => {
+      setPage({
+        ...page,
+        key: keySearch
+      });
+    }, 300);
+   
+  };
+  const deleteListId=()=>{
+    var listId = []
+    for (let index = 0; index < check.length; index++) {
+      if(check[index].checked){
+        listId.push(check[index].getAttribute('id_product'))
+      }
+    }
+    http
+    .post("products/delete-multiple-products", listId)
+    .then(function (response) {
+      setList();
+      swal({
+        title: "Xóa phẩm thành công!",
+        text: "You clicked the button!",
+        icon: "success",
+        button: "Đóng!",
+      });
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    console.log(listId)
+  }
   return (
-    <div className="table-responsive">
-      <div className="d-flex justify-content-end mb-2">
+    <div className="table">
+      <div className="d-flex justify-content-end mb-2 ">
         <button type="button" className="btn btn-light">
           <Link to="/admin/add-product">Thêm mới sản phẩm </Link>
         </button>
+      </div>
+      <div className="row">
+        <div className="form-group col-md-2">
+          <label htmlFor="exampleFormControlSelect1">Lọc theo danh mục</label>
+          <select
+            className="form-control"
+            id="xapxep"
+            onChange={(e) => {
+              setPage({
+                ...page,
+                iddm: e.target.value,
+              });
+            }}
+          >
+            <option value="" selected>
+              Tất cả
+            </option>
+            <option value={0} >
+              Chưa có danh mục
+            </option>
+            {category.map((item, index) => (
+              <option value={item.id}>{item.name_category}</option>
+            ))}
+          </select>
+        </div>
+        <div className="col-md-8">
+          <div className="form-group col-md-6">
+            <label htmlFor="exampleInputEmail1">Tìm Kiếm</label>
+            <input
+              type="text"
+              className="form-control"
+              id="searchsp"
+              aria-describedby="emailHelp"
+              placeholder="Tìm kiếm"
+              onChange={searchKey}
+            />
+          </div>
+        </div>
+        <div className="form-group col-md-2">
+          <label htmlFor="exampleFormControlSelect1">Kích thước</label>
+          <select className="form-control"
+            onChange={(e) => {
+              setPage({
+                ...page,
+                page_size: e.target.value,
+              });
+            }}
+          >
+            <option>Chọn</option>
+            <option value={5}>5 sản phẩm</option>
+            <option value={10}>10 sản phẩm</option>
+            <option value={15}>15 sản phẩm</option>
+            <option value={20}>20 sản phẩm</option>
+          </select>
+        </div>
       </div>
       <table className="table">
         <thead className="thead-light">
           <tr>
             <th scope="col">
               <input type="checkbox" id="checkAll" onClick={checkAll} /> Check
-              all
             </th>
-            <th scope="col">id</th>
+            <th scope="col">Stt</th>
             <th scope="col">Tên sản phẩm </th>
             <th scope="col">Images</th>
             <th scope="col">Giá</th>
@@ -138,7 +256,7 @@ const ShowProducts = ({ addFormData }) => {
               <td>{product.name_product}</td>
               <td>
                 {" "}
-                <img style={{ width: "200px" }} src={product.images} alt="" />
+                <img style={{ width: "120px" }} src={product.images} alt="" />
               </td>
               <td>{product.price}</td>
               <td>{product.amount}</td>
@@ -170,13 +288,14 @@ const ShowProducts = ({ addFormData }) => {
             </tr>
           ))}
         </tbody>
+        <button type="button" onClick = {deleteListId} className="btn btn-danger">Xóa mục đã chọn</button>
       </table>
       <nav aria-label="Page navigation example" id="pagination">
         <ul className="pagination">
           <li className="page-item">
             <button
               disabled={page <= 1}
-              onClick={() => setPage(page - 1)}
+              onClick={() => setPage({ ...page, page: page.page - 1 })}
               className="page-link"
               href="#"
               aria-label="Previous"
@@ -188,7 +307,12 @@ const ShowProducts = ({ addFormData }) => {
           <li className="page-item">
             <button
               disabled={page >= totalpage}
-              onClick={() => setPage(page + 1)}
+              onClick={() =>
+                setPage({
+                  ...page,
+                  page: page.page + 1,
+                })
+              }
               className="page-link"
               href="#"
               aria-label="Next"
